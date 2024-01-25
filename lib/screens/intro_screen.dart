62f0a65dac63f0caaf6fa4c32e88f3c0a1f070b8
Aymin <ayminjf@gmail.com>
2024-01-26 01:47:09 +0330

    feat(intro-screen): add appbar to intro_screen.dart

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -10,6 +10,34 @@ class IntroScreen extends StatefulWidget {
 class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(18),
+              onTap: () {},
+              child: const SizedBox(
+                width: 60,
+                child: Center(
+                  child: Text(
+                    " رد کردن ",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontFamily: "lalezar",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
