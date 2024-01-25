8cdc83b7858563022dc1b01b4a0f42f4565cf9c3
Aymin <ayminjf@gmail.com>
2024-01-26 01:44:03 +0330

    chore(intro-screen): modify main.dart and add screens folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:plant_shop/screens/intro_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Plant Shop',
+      home: IntroScreen(),
     );
   }
 }
