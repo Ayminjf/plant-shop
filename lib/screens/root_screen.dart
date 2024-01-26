9ac075e6d5fc20b43a370a61ea5f1d647f17b166
Aymin <ayminjf@gmail.com>
2024-01-26 22:51:35 +0330

    feat(root-screen): add appbar to root_screen.dart

--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:plant_shop/screens/cart_screen.dart';
+import 'package:plant_shop/screens/favorite_screen.dart';
+import 'package:plant_shop/screens/home_screen.dart';
+import 'package:plant_shop/screens/profile_screen.dart';
 
 class RootScreen extends StatefulWidget {
   const RootScreen({super.key});
@@ -8,11 +12,55 @@ class RootScreen extends StatefulWidget {
 }
 
 class _RootScreenState extends State<RootScreen> {
+  int bottomIndex = 0;
+  List<Widget> screens = const [
+    HomeScreen(),
+    FavoriteScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
+
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.account_box
+  ];
+
+  List<String> appBarTitle = [
+    "خانه",
+    "علاقه‌‌مندی‌ها",
+    "سبد‌خرید",
+    "پروفایل",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Root Page"),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(
+                Icons.notifications,
+                color: Color(0xFF737373),
+                size: 30,
+              ),
+              Text(
+                appBarTitle[bottomIndex],
+                style: const TextStyle(
+                  fontFamily: "lalezar",
+                  color: Color(0xFF737373),
+                  fontSize: 25,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
