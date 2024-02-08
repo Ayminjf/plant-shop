6fadc25c33555ef94b04e676eb41073bc5772498
Aymin <ayminjf@gmail.com>
2024-02-08 19:24:13 +0330

    feat(scan-screen): add appbar

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_shop/constants/constants.dart';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({super.key});
@@ -10,9 +11,52 @@ class ScanScreen extends StatefulWidget {
 class _ScanScreenState extends State<ScanScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("علف اسکن کن"),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50.0,
+            left: 20.0,
+            right: 20.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  // X Button
+                  child: Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Constants.primaryColor.withOpacity(0.15),
+                    ),
+                    child: Icon(
+                      Icons.close,
+                      color: Constants.primaryColor,
+                    ),
+                  ),
+                ),
+                // Share Button
+                Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Constants.primaryColor.withOpacity(0.15),
+                  ),
+                  child: Icon(
+                    Icons.share,
+                    color: Constants.primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
