473c4d4dd913d07ab0329bcb094e18eedf2ddb0b
Aymin <ayminjf@gmail.com>
2024-02-11 21:09:13 +0330

    feat(scan-screen): delete unused package

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:plant_shop/constants/constants.dart';
 import 'package:plant_shop/screens/camera_screen.dart';
