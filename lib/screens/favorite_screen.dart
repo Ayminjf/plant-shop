0172ed8653e34fece56e3594ba1be191afd74de0
Aymin <ayminjf@gmail.com>
2024-02-12 18:56:50 +0330

    feat(favorite-screen): add products into favorite

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plant_shop/models/plant_model.dart';
+import 'package:plant_shop/widgets/plant_widget.dart';
 
 class FavoriteScreen extends StatefulWidget {
   final List<Plant> favoritedPlants;
@@ -15,10 +16,42 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("علاقه مندی ها"),
-      ),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: widget.favoritedPlants.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Image.asset("assets/images/favorited.png"),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "ظاهرا به هیچی علاقه نداشتی :)",
+                    textDirection: TextDirection.rtl,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      fontFamily: "byekan",
+                    ),
+                  )
+                ],
+              ),
+            )
+          : Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 30),
+              height: size.height * 0.8,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return NewPlantWidget(
+                      plantList: widget.favoritedPlants, index: index);
+                },
+                itemCount: widget.favoritedPlants.length,
+              ),
+            ),
     );
   }
 }
