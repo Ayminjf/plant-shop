096d316f8c0d36ca1ad404b6cf19044f573f24a3
Aymin <ayminjf@gmail.com>
2024-02-14 16:45:37 +0330

    feat(cart-screen): display total prices in the shopping cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:plant_shop/constants/constants.dart';
 import 'package:plant_shop/models/plant_model.dart';
+import 'package:plant_shop/widgets/persian_number.dart';
 import 'package:plant_shop/widgets/plant_widget.dart';
 
 class CartScreen extends StatefulWidget {
@@ -15,9 +17,16 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  int sumShoppingCart() {
+    int counter = 0;
+    for (var element in widget.addedToCartPlants) {
+      counter += element.price;
+    }
+    return counter;
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: widget.addedToCartPlants.isEmpty
           ? Center(
@@ -43,14 +52,58 @@ class _CartScreenState extends State<CartScreen> {
             )
           : Container(
               padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 30),
-              height: size.height * 0.8,
-              child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return NewPlantWidget(
-                      plantList: widget.addedToCartPlants, index: index);
-                },
-                itemCount: widget.addedToCartPlants.length,
+              // height: size.height * 0.8,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return NewPlantWidget(
+                            plantList: widget.addedToCartPlants, index: index);
+                      },
+                      itemCount: widget.addedToCartPlants.length,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const Divider(thickness: 1),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                height: 20,
+                                child: Image.asset(
+                                  "assets/images/PriceUnit-green.png",
+                                ),
+                              ),
+                              const SizedBox(width: 5),
+                              Text(
+                                sumShoppingCart().toString().persianNumber,
+                                style: TextStyle(
+                                  fontFamily: "lalezar",
+                                  color: Constants.primaryColor,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Text(
+                            "جمع کل",
+                            style: TextStyle(
+                              fontFamily: "lalezar",
+                              fontSize: 23,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
     );
